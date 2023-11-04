f841acdd6457b7dd6df5c1bd42af162147826ce1
Marco Antonio <marcokjjkk@gmail.com>
2023-11-03 21:03:17 -0300

    começando o corpo principal

--- LISTA3/EXConjunto.c
+++ LISTA3/EXConjunto.c
@@ -8,7 +8,7 @@
 #include <limits.h>
 
 //Define o tamanho máximo de elementos no conjunto
-#define MAX_ELEMENTOS 1000000 
+#define MAX_ELEMENTOS 100000 
 
 //Define as constantes
 #define SUCESSO 1
@@ -64,7 +64,7 @@ int conjuntoVazio(Conjunto *C){
 
 //Função pra iserir um elemento no conjunto
 int insereElementoConjunto(long int x, Conjunto *C){
-    if(C != NULL && C->tamanho < MAX_ELEMENTOS && x < LONG_MAX){
+    if(C != NULL && C->tamanho < MAX_ELEMENTOS && x < LONG_MAX && x > LONG_MIN){
         for(int i = 0; i < C->tamanho; i++){
             if(C->elementos[i] == x){
                 return FALHA; //elemento já existe no conjunto
@@ -86,7 +86,7 @@ int excluirElementoConjunto(long int x, Conjunto *C){
             if(C->elementos[i] == x){
                 encontrado = TRUE;
                 //move os elementos á direita do elemento que vai ser excluido
-                for(int j = 0; j < C->tamanho - 1; i++){
+                for(int j = 0; j < C->tamanho - 1; j++){
                     C->elementos[j] = C->elementos[j+1];
                 }
                 C->tamanho--;
@@ -315,15 +315,146 @@ int destroiConjunto(Conjunto *C){
     return FALHA;
 }
 
+void printLinha(){
+    printf("+------------------------------------------------------------------------+\n");
+}
+
+void opcoes(){
+    printLinha();
+    printf("| [1 ] -> [Criar um conjunto C]                                          |\n");
+    printf("| [2 ] -> [Verificar se um conjunto é vazio]                             |\n");
+    printf("| [3 ] -> [Inserir um elemento no conjunto]                              |\n");
+    printf("| [4 ] -> [Excluir um elemento do conjunto]                              |\n");
+    printf("| [5 ] -> [Mostrar os elementos de um conjunto]                          |\n");
+    printf("| [6 ] -> [Calcular a cardinalidade de um conjunto]                      |\n");
+    printf("| [7 ] -> [Verificar elementos maiores que um número]                    |\n");
+    printf("| [8 ] -> [Verificar elementos menores que um número]                    |\n");
+    printf("| [9 ] -> [Verificar pertinência de um elemento]                         |\n");
+    printf("| [10] -> [Comparar conjuntos idênticos]                                 |\n");
+    printf("| [11] -> [Identificar subconjunto]                                      |\n");
+    printf("| [12] -> [Gerar complemento de um conjunto]                             |\n");
+    printf("| [13] -> [Gerar união de dois conjuntos]                                |\n");
+    printf("| [14] -> [Gerar intersecção entre dois conjuntos]                       |\n");
+    printf("| [15] -> [Gerar diferença entre dois conjuntos]                         |\n");
+    printf("| [16] -> [Gerar conjunto das partes]                                    |\n");
+    printf("| [17] -> [Copiar conjunto para outro]                                   |\n");
+    printf("| [18] -> [Destruir conjunto]                                            |\n");
+}
+
+void boasVindas(){
+    printLinha();
+    printf("|                    Bem-vindo ao menu de operações                      |\n");
+    printf("|                            com Conjuntos!                              |\n");
+    printLinha();
+    printf("|                          Escolha uma opção:                            |\n");
+    opcoes();
+}
+
 //==============================================================================
 // Corpo Principal
 //==============================================================================
 
-#include <stdio.h>
-
 int main(){
+    boasVindas();
+    int escolha;
+    while(escolha != 0){
+        Conjunto C;
+        printLinha();
+        printf("| [999] -> [Mostrar opções novamente]                                    |\n");
+        printf("| [ 0 ] -> [Encerrar programa]                                           |\n");
+        printLinha();
 
-    
+        scanf("%d", &escolha);
+        switch (escolha) {
+            case 0:
+                break;
+            case 999:
+                opcoes();
+                break;
+            case 1: {
+                int criar = criaConjunto(&C);
+                (criar == 1) ? printf("Conjunto criado com sucesso\n") : printf("Erro na criação\n");
+                break;
+            }
+            case 2: {
+                int vazio = conjuntoVazio(&C);
+                (vazio == 1) ? printf("Conjunto é vazio\n") : printf("Conjunto não é vazio\n");
+                break;
+            }
+            case 3: {
+                long int elemento;
+                printf("Digite o elemento a ser inserido no conjunto: ");
+                scanf("%ld", &elemento);
+                int inserido = insereElementoConjunto(elemento, &C);
+                (inserido == 1) ? printf("Elemento inserido com sucesso\n") : printf("Erro na inserção do elemento\n");
+                break;
+            }
+            
+            case 4: {
+                long int elemento;
+                printf("Digite o elemento a ser excluido do conjunto: ");
+                scanf("%ld", &elemento);
+                int excluido = excluirElementoConjunto(elemento, &C);
+                (excluido == 1) ? printf("Elemento excluido com sucesso\n") : printf("Erro na exclusão do elemento\n");
+                break;
+            }
+
+            case 5: {
+                printf("Mostrar conjunto de forma Crescente[1] ou Descrescente[2]?: ");
+                int cres;
+                scanf("%d", &cres);
+                switch (cres) {
+                    case 1:
+                        mostraConjunto(&C, CRESCENTE);
+                        break;
+                    case 2:
+                        mostraConjunto(&C, DECRESCENTE);
+                        break;
+                    default:
+                        printf("Forma inválida, tente novamente!\n");
+                        break;
+                }
+                break;
+            }
+            case 6: {
+                int tamanho = tamanhoConjunto(&C);
+                printf("Tamanho do conjunto: %d\n", tamanho);
+                break;
+            }
+
+            case 7: {
+                long int elemento;
+                printf("Digite um elemento: ");
+                scanf("%ld", &elemento);
+                int maiores = maior(elemento, &C);
+                printf("Elementos maiores que o número dado: %d\n", maiores);
+                printf("(O valor 0 (zero) indica que todos os elementos de C são maiores que x)\n");
+                break;
+            }
+            case 8: {
+                long int elemento;
+                printf("Digite um elemento: ");
+                scanf("%ld", &elemento);
+                int menores = menor(elemento, &C);
+                printf("Elementos menores que o número dado: %d\n", menores);
+                printf("(O valor 0 (zero) indica que todos os elementos de C são maiores que x)\n");
+                break;
+            }
+            case 9: {
+                long int elemento;
+                printf("Digite um elemento: ");
+                scanf("%ld", &elemento);
+                int pertence = pertenceConjunto(elemento, &C);
+                (pertence == 1) ? printf("O elemento dado pertence ao conjunto\n") : printf("O elemento dado não pertence ao conjunto\n");
+                break;
+            }
+
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+                break;
+        }
+    }
+
 
     return 0;
 }
